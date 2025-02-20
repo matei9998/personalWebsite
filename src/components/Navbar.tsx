f0004ec317549f196b0c32127b23c9d4a0cd1577
matei9998 <barbalatamatei12@gmail.com>
2025-02-20 01:17:34 +0100

    navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 w-full flex justify-between items-center p-4 text-white text-lg">
+      <Link
+        to="/"
+        className="font-bold text-2xl hover:text-blue-400 transition-all"
+      >
+        Matei Barbalata
+      </Link>
+
+      <div className="flex space-x-9 mr-16">
+        {["About", "Projects", "Sandbox"].map((item) => (
+          <Link
+            key={item}
+            to={`/${item.toLowerCase()}`}
+            className="hover:text-blue-400 transition-all"
+          >
+            {item}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
